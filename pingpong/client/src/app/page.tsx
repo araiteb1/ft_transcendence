f5e6fdf0906289d2ab36c9028f1240d5d9f2d9b8
Ahlam Raiteb <araiteb@e2r11p1.1337.ma>
2024-05-21 16:01:57 +0100

    debut

---
+++ pingpong/client/src/app/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css'
+
+
+function App()  {
+  return (
+    <div className="container ">
+      <div className="logo"> logo </div>
+      <div className="header"> header </div>
+      <div className="nav"> nav </div>
+      <div className="content"> content </div>
+      <div className="footer"> footer </div>
+    </div>
+  );
+
+}
+
+export default App;
