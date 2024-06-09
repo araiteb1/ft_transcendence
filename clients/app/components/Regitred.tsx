9ab19897660aa0553abee9236f672657c22a0930
Ahlam Raiteb <araiteb@e2r13p1.1337.ma>
2024-06-09 11:37:54 +0100

    registed-page

--- clients/app/components/Regitred.tsx
+++ clients/app/components/Regitred.tsx
@@ -1,4 +1,4 @@
-import {EmailIcon, SchoolIcon}from "../icons/Icons";
+import {EmailIcon}from "../icons/Icons";
 
 const Regitred = () => {
     return (
@@ -6,17 +6,28 @@ const Regitred = () => {
           
           <div className=" border  bg-[#C2B287] w-[20px] h-[150px] rounded-lg  ">
           </div>
-            <div className=" flex  absolute inset-x-5 top-[10%] text-[#FFFFFF] w-[45%] h-[80%] ">
+            <div className="  flex  absolute inset-x-5 top-[10%] text-[#FFFFFF] w-[45%] h-[80%] ">
                   <h1 className=" text-center text-[32px] absolute left-[30%]">Continue With</h1>
-                  <button className=" flex border w-[40px] h-[40px] bg-[#ffffff]">
+                    <button className=" items-center absolute inset-x-40 top-[15%] flex border w-[40px] h-[40px] bg-[#ffffff]">
                       <EmailIcon />
                     </button>
-                  
+                    <button className=" absolute inset-x-80 top-[15%] flex border w-[40px] h-[40px] bg-[#ffffff] rounded-full">
+                      <p className=" text-[#181818]  items-center text-[30px] t">42</p>
+                    </button>
+                  <h2 className="absolute inset-x-44 top-[25%] text-center text-[32px] self-center left-[30%]"> Or</h2>
+                  <div className=" absolute inset-x-20 top-[40%] border bg-MainButon w-[60%] h-[8%] self-center rounded-3xl text-[#181818] m-2">
+                    <p className="m-2">email</p>
+                    </div>
+                  <div className="absolute inset-x-20 top-[53%] border bg-MainButon w-[60%] h-[8%] self-center rounded-3xl  text-[#181818] m-2">
+                    <p className="m-2">password</p></div>
             </div> 
           <div className=" border absolute inset-y-0 left-[50%] border-[#C2B287] w-[2px] h-full">
           </div>
+          <div className="flex-col w-[45%] h-[full] absolute inset-y-0 right-5 top-[25%] items-center justify-center">
+              <p className="text-[#FFFFFF] text-[40px] text-center "> Don't have an account ?</p>
+              <button className="px-12 py-5 w-full sm:w-fit rounded-lg bg-[#553D1E] text-[25px] m-20">Sing Up</button>
+          </div>
           <div className=" border absolute inset-y-0 right-0 flex-col bg-[#C2B287]  w-[20px] h-[150px] rounded-lg">
-
           </div>
         </div>
     );
