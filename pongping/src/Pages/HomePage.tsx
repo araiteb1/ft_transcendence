153fc409394f4b71a20d7e13d454e1a74780a013
Ahlam Raiteb <araiteb@e3r3p12.1337.ma>
2024-06-30 16:34:39 +0100

    homePage

--- pongping/src/Pages/HomePage.tsx
+++ pongping/src/Pages/HomePage.tsx
@@ -2,15 +2,19 @@ import Navbarp from "../Components/Navbarp";
 
 export default function HomePage ()  {
     return (
-        <div className="flex flex-col w-full space-y-1">
+        <div className="flex flex-col items-center justify-center w-full space-y-1">
             <Navbarp />
             <div className="flex flex-col items-center"
                 style={{
                     backgroundImage: `url(${'/images/img1.png'})`,
                     backgroundRepeat: 'no-repeat',
                     backgroundSize:"cover",
-                    width:"2000px",
-                    height:"700px"
+                    maxWidth:"1800px",
+                    maxHeight:"1000px",
+                    width:"1270px",
+                    height:"600px",
+                    minWidth:"100px",
+                    minHeight:"100px"
             }} >
                 <div className=" flex self-center broder border-TextColor1 w-[90%] h-[10%]  bg-SubMain rounded-full">
                     <img src="/images/Logo.png " width={30} height={50} className=" m-2 pl-1 "></img>
--- pongping/src/Pages/ProfilPage.tsx
+++ pongping/src/Pages/ProfilPage.tsx
@@ -5,34 +5,37 @@ export default function ProfilPage ()  {
     return (
         <div className=" flex flex-col items-center   w-[100%] space-y-2">
             <Navbarp />
-            <span className=" flex flex-col  w-[80%] h-full">
-                <div className=" flex flex-col items-start w-[100%]">
-                    <div className=" flex justify-center items-center menu   border 
-                                        border-TextColor2 bg-SubMain w-[80%] h-[90px] rounded-3xl 
-                                        space-x-8 text-[30px] text-TextColor1">
-                        <Link to="../Components/OverVieW" > OverVieW </Link>
-                        <Link to="../Components/Avievement" > Avievement </Link>
-                        <Link to="../Components/ MatchHistory" > Match History </Link>
-                        <Link to="../Components/Rank" > Rank </Link>
-                        <Link to="../Components/Stats" > Stats </Link>
-                    </div>
-                    <div  className="flex flex-col "
-                        style={{
-                            backgroundImage: `url(${`/images/fan.png`})`,
-                            backgroundRepeat: 'no-repeat',
-                            backgroundSize:"contain",
-                            maxWidth:"1800px",
-                            width:"1100px",
-                            height:"1500px"
-                        }}>
-                        <div className=" border border-Component1 bg-SubMain w-[250px] h-[650px] rounded-b-full m-9">
+            <span className=" flex  w-[80%] h-full min-w-[120px] ">
+                    <div className=" flex flex-col items-start w-[100%]">
+                        <div className=" flex justify-center items-center menu   border 
+                                            border-TextColor2 bg-SubMain w-[80%] h-[90px] rounded-3xl 
+                                            space-x-8 text-[30px] text-TextColor1">
+                            <Link to="../Components/OverVieW" > OverVieW </Link>
+                            <Link to="../Components/Avievement" > Avievement </Link>
+                            <Link to="../Components/MatchHistory" > Match History </Link>
+                            <Link to="../Components/Rank" > Rank </Link>
+                            <Link to="../Components/Stats" > Stats </Link>
                         </div>
-                        </div>
-                </div>
-                <div className=" flex items-end border border-Component1 w-[60px] h-[300px] bg-BackGround text-[#ffffff] text-[24px]">
-                                erghjklghjko ftghuji ygou bh 
-                </div>
-            </span>
+                        <div  className="flex flex-col min-w-[120px]"
+                            style={{
+                                backgroundImage: `url(${`/images/fan.png`})`,
+                                backgroundRepeat: 'no-repeat',
+                                backgroundSize:"contain",
+                                maxWidth:"1800px",
+                                maxHeight:"1000px",
+                                width:"1100px",
+                
+                                minWidth:"100px",
+                                minHeight:"100px"
+                            }}>
+                                <div className=" border border-Component1 bg-SubMain w-[250px] h-[650px] rounded-b-full m-9">
+                                </div>
+                         </div>
+                                    <div className=" flex self-end  border  border-Component1 w-[60px] h-[300px] bg-BackGround text-[#ffffff] text-[24px]">
+                                                
+                                    </div>
+                            </div>
+                </span>
         </div>
     );
 }
